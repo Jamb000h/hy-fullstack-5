0e69cbaa9ea280fe4eb8a9866323b00fbc0d3e38
Jonne Kanerva <jonne.kanerva@siili.com>
2018-08-17 12:23:12 +0300

    tehtävä 5.6

--- 5.1-5.16/bloglist-frontend/src/components/Blog.js
+++ 5.1-5.16/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,44 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false,
+    }
+  }
+
+  toggleVisibility = () => {
+    this.setState({visible: !this.state.visible})
+  }
+
+  render() {
+
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+
+    const blog = this.props.blog
+    const showWhenVisible = { display: this.state.visible ? '' : 'none' }
+
+    return (
+      <div style={blogStyle}>
+        <div onClick={this.toggleVisibility}>
+          {blog.title} {blog.author}
+        </div>
+        <div style={showWhenVisible}>
+          {blog.url}
+          {blog.likes}
+          <button>vote</button>
+          added by {blog.user.name}
+        </div>
+      </div>  
+    );
+  }
+}
 
 export default Blog
