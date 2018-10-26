0c620df027c081a48eeefd0a81f0d212cd39ad2c
Jonne Kanerva <jonne.kanerva@siili.com>
2018-10-26 15:24:04 +0300

    Tehtävä 5.18

--- 5.17-5.18/unicafe-redux/src/index.js
+++ 5.17-5.18/unicafe-redux/src/index.js
@@ -1,5 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {createStore} from 'redux'
+import counterReducer from './palauteReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counterReducer)
+
+const Otsikko = props => <h2>{props.teksti}</h2>
+
+const Button = props => {
+  return (
+    <button onClick={props.onClick}>{props.teksti}</button>
+  )
+}
+
+const PalauteLista = () => {
+  return (
+    <tbody>
+      <Palaute teksti="hyvä" arvo={store.getState().good} />
+      <Palaute teksti="neutraali" arvo={store.getState().ok} />
+      <Palaute teksti="huono" arvo={store.getState().bad} />
+    </tbody>
+  )
+}
+
+const Palaute = props => {
+  return (
+    <tr>
+      <td>{props.teksti}</td>
+      <td>{props.arvo}</td>
+    </tr>
+  )
+}
+
+const Statistic = () => {
+
+  const data = store.getState();
+
+  const palautteita = Object.values(data).reduce( (yht, palaute) => yht + palaute, 0)
+  const positiivisia = data.good / palautteita * 100
+
+  return (
+    <tr>
+      <td>positiivisia</td>
+      <td>{palautteita > 0 ? positiivisia.toFixed(1) : 0} %</td>
+    </tr>
+  )
+}
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Otsikko teksti="anna palautetta" />
+        <Button teksti="hyva" onClick={e => store.dispatch({ type: 'GOOD' })} />
+        <Button teksti="neutraali" onClick={e => store.dispatch({ type: 'OK' })} />
+        <Button teksti="huono" onClick={e => store.dispatch({ type: 'BAD' })} />
+        <Otsikko teksti="statistiikka" />
+        <table>
+          <PalauteLista />
+          <Statistic />
+        </table>
+        <Button teksti="nollaa" onClick={e => store.dispatch({ type: 'ZERO' })} />
+      </div>
+    );
+  }
+
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
--- 5.17-5.18/unicafe-redux/src/palauteReducer.js
+++ 5.17-5.18/unicafe-redux/src/palauteReducer.js
@@ -10,13 +10,14 @@ const counterReducer = (state = initialState, action) => {
     case 'GOOD':
       return {...state, good: state.good + 1}
     case 'OK':
-    return {...state, ok: state.ok + 1}
+      return {...state, ok: state.ok + 1}
     case 'BAD':
-    return {...state, bad: state.bad + 1}
+      return {...state, bad: state.bad + 1}
     case 'ZERO':
+      return {...state, good: 0, ok: 0, bad: 0}
+    default: 
       return state
   }
-  return state
 }
 
 export default counterReducer
